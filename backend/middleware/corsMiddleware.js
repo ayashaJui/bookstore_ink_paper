6e43c73e4538c47ac83841ffb2d3f2dfaf34b578
ayashaJui <jui1801@cseku.ac.bd>
2023-07-13 22:41:52 +0600

    Deploying

--- backend/middleware/corsMiddleware.js
+++ backend/middleware/corsMiddleware.js
@@ -1,5 +1,8 @@
 const corsMiddleware = (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://inkandpaper.onrender.com"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", true);
